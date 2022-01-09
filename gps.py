088893533c4e3216c770d49c26c1be4ed567ea20
MaxGomas <mx.gomas@gmail.com>
2022-01-09 17:32:07 +0100

    Test github.dev

--- gps.py
+++ gps.py
@@ -12,6 +12,8 @@ import re
 pattern = re.compile("^([N|S|E|W][0-9]+°[0-9]+'[0-9\.]+'')+$")
 
 
+# Experiencing github.dev
+
 parser = argparse.ArgumentParser(description="I need longidute and latitude. (accpted formats : DD, DMS [N|S|E|W]X°XX'XX.XX'')")
 parser.add_argument("latitude", help="DD or DMS latitude in string")
 parser.add_argument("longitude", help="DD or DMS longitude in string")
